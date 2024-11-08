57dcd1558fc0933ca612a91038ec70c02e5b14c8
Boundy2739 <boundy42@gmail.com>
2024-11-08 14:17:31 +0000

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,8 @@
+number1 = int(input("Insert a number\n"))
+number2 = int(input("Insert the second number\n"))
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
---
+++ grades.py
@@ -0,0 +1,18 @@
+
+score = int(input("Please insert your score:"))
+
+if score > 69 and score <101:
+            print("Your grade is A")
+elif score < 70 and score >59:
+            print("Your grade is B")
+elif score < 60 and score > 49:
+            print("Your score is C")
+elif score < 50 and score > 39:
+            print("Your grade is D")
+elif score < 40 and score > 29:
+            print("Your score is E")
+elif score < 30 and score > 19:
+            print("Your score is F")
+            
+    
+
